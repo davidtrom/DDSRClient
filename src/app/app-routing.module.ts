a2eb715e242e2b470aa8d11d4e2cdddfd1ad3775
David Trombello <dtrombello@gmail.com>
2020-02-17 15:37:05 -0500

    added links in header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { StampedeComponent } from './stampede/stampede.component';
+import { MileageMattersComponent } from './mileage-matters/mileage-matters.component';
+import { ScholarshipComponent } from './scholarship/scholarship.component';
+import { RunningHomeComponent } from './running-home/running-home.component';
 
 
 const routes: Routes = [
   { path: '', component: DashboardComponent},
+  { path: 'stampede', component: StampedeComponent},
+  { path: 'mileage-matters', component: MileageMattersComponent},
+  { path: 'scholarship', component: ScholarshipComponent},
+  { path: 'running', component: RunningHomeComponent},
   // if no route, redirect to home
   { path: '**', redirectTo: '' }
 
